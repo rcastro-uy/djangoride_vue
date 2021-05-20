ed3f8c978bf4178528c3b5be2a46edbef69f9aff
Rodrigo Castro <rcastro@netascom.com>
2021-05-20 00:36:36 -0300

    Menu Hamburguesa para Mobile

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,16 @@
           aria-label="menu"
           aria-expanded="false"
           data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-
-      <div class="navbar-menu" id="navbar-menu">
+      
+<!-- vue directive v-bind:class="{'is-active': showMobileMenu}"-->
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
           <router-link to="/bicicletas" class="navbar-item"
             >Bicicletas</router-link
@@ -52,6 +54,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  },
+}
+</script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
