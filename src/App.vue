a27f2dc6a7bc0719288f0c4183d6ed890952614e
Rodrigo Castro <rcastro@programmer.net>
2021-05-27 14:34:19 -0300

    Headers de Authentication en App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@
 
           <div class="navbar-item">
             <div class="buttons">
-              <router-link to="/log-in" class="button is-light">
+              <router-link to="/login" class="button is-light">
                 Log in</router-link
               >
 
@@ -86,6 +86,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -97,6 +98,14 @@ export default {
   },
   beforeCreate() {
     this.$store.commit('initStore')
+
+    const token = this.$store.state.token
+
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
+    }
   },
   mounted(){
     this.cart = this.$store.state.cart
